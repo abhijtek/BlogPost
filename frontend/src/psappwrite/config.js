5c079385594f28f6f42e04e2bd9c8f92dea0ee4e
abhijtek <singhabhijeet1212@gmail.com>
2026-01-19 02:01:50 +0530

    feat: setUp store for state

--- frontend/src/psappwrite/config.js
+++ frontend/src/psappwrite/config.js
@@ -1,3 +1,48 @@
 import conf from "../conf/conf";
 
-class 
+import { Client, Storage, ID } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://<REGION>.cloud.appwrite.io/v1')
+    .setProject(conf.appwriteProjectId);
+
+const storage = new Storage(client);
+
+
+
+class Service{
+    async uploadFile(file){
+        try {
+            return await storage.createFile({
+    bucketId: conf.appwriteBucketId,
+    fileId: ID.unique(),
+    file: file
+});
+        } catch (error) {
+            console.log("appwrite file service :: upload file",error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            const result = await storage.deleteFile({
+    bucketId: conf.appwriteBucketId,
+    fileId: fileId
+});
+        } catch (error) {
+            console.log("appwrite file error::delete::",error);
+        }
+    }
+
+    getFilePreview(fileId){
+        try {
+            return storage.getFilePreview({
+                bucketId: appwriteBucketId,
+    fileId: fileId,
+            })
+        } catch (error) {
+            
+        }
+    }
+}
